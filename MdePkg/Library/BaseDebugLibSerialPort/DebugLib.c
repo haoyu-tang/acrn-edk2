6a6115ab616caefebac2f0e3862c71c7676b232e
Peter Fang <peter.fang@intel.com>
2019-03-12 02:12:09 -0700

    Remove debug code in release build

--- MdePkg/Library/BaseDebugLibSerialPort/DebugLib.c
+++ MdePkg/Library/BaseDebugLibSerialPort/DebugLib.c
@@ -323,7 +323,11 @@ DebugCodeEnabled (
   VOID
   )
 {
+#if !defined(MDEPKG_NDEBUG)
   return (BOOLEAN)((PcdGet8 (PcdDebugPropertyMask) & DEBUG_PROPERTY_DEBUG_CODE_ENABLED) != 0);
+#else
+  return FALSE;
+#endif
 }
 
 /**
